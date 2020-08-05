6e1adf6e364fa33396d5ec879ce0e0596fa5f2a7
carlybrail <69254570+carlybrail@users.noreply.github.com>
2020-08-05 11:33:37 -0400

    Add files via upload
    my first Xcode project

---
+++ storyboardintro/storyboardintro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  storyboardintro
+//
+//  Created by Carly Brail on 8/5/20.
+//  Copyright © 2020 Carly Brail. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    print("this is a print statement- testing testing")
+    }
+
+
+}
+
